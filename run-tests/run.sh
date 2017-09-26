f39d6cd4ef8da439adcafad704721bd4896f113e
Megan Gaebler <megan.gaebler@gmail.com>
2017-09-25 22:10:07 -0500

    Add first two tasks

---
+++ run-tests/run.sh
@@ -0,0 +1,4 @@
+echo "You done did start the test"
+sleep 5
+echo "The tests did done pass!"
+exit 0
---
+++ tag-prerelease/run.sh
@@ -0,0 +1,7 @@
+server_dir = "$(pwd) source-code"
+version = "$(cat version/number)"
+
+pushd $server_dir
+	git tag $version
+	git push --tags origin master
+popd
