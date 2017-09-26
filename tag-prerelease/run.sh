bd0a8afbe74ff7799bec461cf8d89de7770727bf
Megan Gaebler <megan.gaebler@gmail.com>
2017-09-26 06:25:52 -0500

    use local tmp

--- tag-prerelease/run.sh
+++ tag-prerelease/run.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-touch /tmp/ssh_key
+mkdir tmp
+touch tmp/ssh_key
 
-cat $ssh_key > /tmp/ssh_key
+cat $ssh_key > tmp/ssh_key
 
 server_dir="$(pwd)/source-code"
 version="$(cat version/number)"
 
 pushd $server_dir
 	git tag $version
-	GIT_SSH_COMMAND='ssh -i /tmp/ssh_key' git push --tags origin master
+	GIT_SSH_COMMAND='ssh -i tmp/ssh_key' git push --tags origin master
 popd
